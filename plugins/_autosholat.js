ba0649df4c12156d283ef5bb0c020e5ad644597c
CREATOR BOT WA <98082226+LynnXzy@users.noreply.github.com>
2024-02-10 17:04:26 +0800

    Update _autosholat.js

--- plugins/_autosholat.js
+++ plugins/_autosholat.js
@@ -29,7 +29,7 @@ before: async function (m) {
 
     for (const [sholat, waktu] of Object.entries(jadwalSholat)) {
         if (timeNow === waktu && !(id in this.autosholat)) {
-            let caption = `Hai kak @${who.split`@`[0]},\nWaktu *${sholat}* telah tiba, ambilah air wudhu dan segeralah shalat.\n\n*${waktu}*\n_untuk wilayah Jakarta dan sekitarnya._`
+            let caption = `Assalammualaikum @${who.split`@`[0]},\nWaktu *${sholat}* telah tiba, ambilah air wudhu dan segeralah shalat.\n\n*${waktu}*\n_untuk wilayah Kuching dan sekitarnya._`
             this.autosholat[id] = [
                 this.reply(m.chat, caption, null, {
                     contextInfo: {
