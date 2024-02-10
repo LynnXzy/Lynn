9a997fc6ab5c552db023c6cfbdf0df7459339b07
CREATOR BOT WA <98082226+LynnXzy@users.noreply.github.com>
2024-02-10 17:11:19 +0800

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
 let ye = `@${m.sender.split`@`[0]}`
 let esce = `
-Hai ${ye} Bot Ini Menggunakan Script :\n• https://github.com/BOTCAHX/RTXZY-MD
+Hai Kntol ${ye} 
 `
 m.reply(esce)
 }
