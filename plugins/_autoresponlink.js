9d405863c88ff4356c72f511977d965332d0ded3
CREATOR BOT WA <98082226+LynnXzy@users.noreply.github.com>
2024-02-10 17:06:19 +0800

    Update _autoresponlink.js

--- plugins/_autoresponlink.js
+++ plugins/_autoresponlink.js
@@ -5,7 +5,7 @@ handler.all = async function (m, { isBlocked }) {
     if (isBlocked) return
     if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('Undangan untuk bergabung') || m.text.startsWith('Invitation to join') || m.text.startsWith('Buka tautan ini')) && !m.isBaileys && !m.isGroup) {
     let teks = `Invite Group
-• 30 Day / Rp 10k
+• 30 Day / Rm 10
 Jika berminat hubungi: @${global.owner[0]} untuk order:)
 `
     this.reply(m.chat, teks, m)
